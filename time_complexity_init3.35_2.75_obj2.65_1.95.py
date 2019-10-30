35508e3eb13212c59398e5f6a730704aa5a5eced
wxadssxgn <19493864@qq.com>
2019-10-31 03:33:40 +0800

    time complexity

---
+++ time_complexity_init3.35_2.75_obj2.65_1.95.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.load('time_complexity_init3.35_2.75_obj2.65_1.95.npy')
+means = sum(a) / len(a)
+b = []
+b.append(means)
+b = b * len(a)
+
+fig = plt.figure(figsize=(9,5))
+plt.plot(a, color='blue', linewidth=2.2, label='Real-time')
+plt.plot(b, color='red', linewidth=2.2, label='Mean')
+plt.legend(fontsize=14, loc='best')
+plt.grid(True, linestyle='-.')
+plt.xlim((0, 100))
+plt.ylim((0, 1400))
+plt.xlabel('Iterations', size=16)
+plt.ylabel('Numbers of States Traversed', size=16)
+plt.show()
+
+print('end')
