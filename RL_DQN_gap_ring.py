2e3b1673d7d314354084bd39b987f58b73e93acf
wxadssxgn <19493864@qq.com>
2019-11-10 14:53:25 +0800

    1

--- RL_DQN_gap_ring.py
+++ RL_DQN_gap_ring.py
@@ -11,13 +11,14 @@ import matplotlib.pyplot as plt
 class model(nn.Module):
     def __init__(self, ):
         super(model, self).__init__()
-        self.layer1 = nn.Sequential(nn.Linear(2, 10), nn.Dropout(0), nn.ReLU(inplace=True))
+        self.layer1 = nn.Sequential(nn.Linear(4, 10), nn.Dropout(0), nn.ReLU(inplace=True))
         self.layer2 = nn.Sequential(nn.Linear(10, 40), nn.Dropout(0), nn.ReLU(inplace=True))
         self.layer3 = nn.Sequential(nn.Linear(40, 100), nn.Dropout(0), nn.ReLU(inplace=True))
         self.layer4 = nn.Sequential(nn.Linear(100, 100), nn.Dropout(0), nn.ReLU(inplace=True))
-        self.layer5 = nn.Sequential(nn.Linear(100, 40), nn.Dropout(0), nn.ReLU(inplace=True))
-        self.layer6 = nn.Sequential(nn.Linear(40, 10), nn.Dropout(0), nn.ReLU(inplace=True))
-        self.layer7 = nn.Sequential(nn.Linear(10, 4))
+        self.layer5 = nn.Sequential(nn.Linear(100, 100), nn.Dropout(0), nn.ReLU(inplace=True))
+        self.layer6 = nn.Sequential(nn.Linear(100, 40), nn.Dropout(0), nn.ReLU(inplace=True))
+        self.layer7 = nn.Sequential(nn.Linear(40, 10), nn.Dropout(0), nn.ReLU(inplace=True))
+        self.layer8 = nn.Sequential(nn.Linear(10, 8))
 
     def forward(self, x):
         x = self.layer1(x)
@@ -27,28 +28,30 @@ class model(nn.Module):
         x = self.layer5(x)
         x = self.layer6(x)
         x = self.layer7(x)
+        x = self.layer8(x)
         return x
+
 
 class DQN():
     def __init__(self, ):
-        spectrum = np.load(file='HYB_s.npy')
-        spectrum = self.get_s_para(spectrum)
-        self.spectrum = spectrum
-        geometry = np.load(file='HYB_g.npy')[:, 0: 2]
-        self.geometry = geometry[:, np.newaxis, :]
+        spectrum = np.load(file='spec_fss_gap_ring.npy')
+        self.spectrum = self.get_s_para(spectrum)
+        geometry = np.load(file='geo_fss_gap_ring.npy')
+        self.geometry = geometry[:, 1: ]
         obj_index = self.get_index(obj)
         init_index = self.get_index(init)
         self.obj_spec = self.spectrum[obj_index]
         self.init_spec = self.spectrum[init_index]
-        self.n_states = 2
-        self.actions = ['R+0.05', 'R-0.05', 'r+0.05', 'r-0.05']
+        self.n_states = 4
+        self.actions = ['SubWidth+0.2', 'SubWidth-0.2', 'RadiusOuter+0.2', 'RadiusOuter-0.2', \
+                        'RadiusInner+0.2', 'RadiusInner-0.2', 'GapLength+0.2', 'GapLength-0.2']
         self.epsilon = 0.9
         self.greedy_epsilon = 1
         self.alpha = 0.1
         self.gamma = 0.9
-        self.memory_capacity = 200
+        self.memory_capacity = 500
         self.LR = 1e-4
-        self.batch_size = 50
+        self.batch_size = 300
 
         self.eval_model = model().cuda() if torch.cuda.is_available() else model()
         self.target_model = model().cuda() if torch.cuda.is_available() else model()
@@ -72,7 +75,7 @@ class DQN():
 
     def get_index(self, s):
         for i in range(len(self.geometry)):
-            if (self.geometry[i] == np.array(s).astype(float).round(2).reshape(1, 2)).all():
+            if (self.geometry[i] == np.array(s).astype(float).round(2).reshape(1, 4)).all():
                 return i
 
     def get_s_para(self, s_parameter):
@@ -80,8 +83,8 @@ class DQN():
         for i in range(s_parameter.shape[0]):
             temp = np.zeros([s_parameter.shape[1], 2])
             for j in range(s_parameter.shape[1]):
-                temp[j][0] = abs(complex(s_parameter[i][:, 1][j], s_parameter[i][:, 2][j]))
-                temp[j][1] = abs(complex(s_parameter[i][:, 3][j], s_parameter[i][:, 4][j]))
+                temp[j][0] = abs(complex(s_parameter[i][:, 0][j], s_parameter[i][:, 1][j]))
+                temp[j][1] = abs(complex(s_parameter[i][:, 2][j], s_parameter[i][:, 3][j]))
             s_abs[i] = temp
         return s_abs
     
@@ -91,7 +94,7 @@ class DQN():
             action_values = self.eval_model.forward(s)
             a_index = torch.max(action_values, 1)[1].cpu().numpy()
         else:
-            a_index = np.random.randint(0, 4, size=(1, ))
+            a_index = np.random.randint(0, 8, size=(1, ))
         return a_index
 
     def greedy_choose_action(self, s, i):
@@ -100,7 +103,7 @@ class DQN():
             action_values = self.eval_model.forward(s)
             a_index = torch.max(action_values, 1)[1].cpu().numpy()
         else:
-            a_index = np.random.randint(0, 4, size=(1, ))
+            a_index = np.random.randint(0, 8, size=(1, ))
         if i % 500 == 0:
             self.greedy_epsilon = self.greedy_epsilon * 0.9
         if self.greedy_epsilon < 0.1:
@@ -109,25 +112,50 @@ class DQN():
     
     def env_feedback(self, s, a):
         # s.type = Tensor
+        # s.shape = [SubWidth, RadiusOuter, RadiusInner, GapLength]
+        # SubWidth > 2*RingRadius_Outer
+        # RingRadius_Outer > RingRadius_Inner
+        # 2*RingRadius_Inner > GapLength
+        # SubThickness: [0.1, 0.2]
+        # SubWidth: [7, 9] step: 0.2
+        # RingRadius_Outer: [1.4, 4.2] step: 0.2
+        # RingRadius_Inner: [1.0, 3.6] step: 0.2
+        # GapLength: [0.5, 5.5] step: 0.2
+        # RingHeight = 0.05
+        # s[0, 0] = SubWidth
+        # s[0, 1] = RadiusOuter
+        # s[0, 2] = RadiusInner
+        # s[0, 3] = GapLength
         s_ = s.clone()
         if a == 0:
-            s_[0, 0] = s_[0, 0] + 0.05
+            s_[0, 0] = s_[0, 0] + 0.2
         elif a == 1:
-            s_[0, 0] = s_[0, 0] - 0.05
+            s_[0, 0] = s_[0, 0] - 0.2
         elif a == 2:
-            s_[0, 1] = s_[0, 1] + 0.05
+            s_[0, 1] = s_[0, 1] + 0.2
+        elif a == 3:
+            s_[0, 1] = s_[0, 1] - 0.2
+        elif a == 4:
+            s_[0, 2] = s_[0, 2] + 0.2
+        elif a == 5:
+            s_[0, 2] = s_[0, 2] - 0.2
+        elif a == 6:
+            s_[0, 3] = s_[0, 3] + 0.2
         else:
-            s_[0, 1] = s_[0, 1] - 0.05
+            s_[0, 3] = s_[0, 3] - 0.2
         tmp_s = s_.cpu().numpy().round(2)
-        if tmp_s[0, 1] >= tmp_s[0, 0] or tmp_s[0, 0] > 3.80 or tmp_s[0, 1] < 1.00:
+        if tmp_s[0, 0] > 9.00 or tmp_s[0, 0] < 7.00 or tmp_s[0, 1] > 4.20 or tmp_s[0, 1] < 1.40 \
+           or tmp_s[0, 2] > 3.60 or tmp_s[0, 2] < 1.00 or tmp_s[0, 3] > 5.50 or tmp_s[0, 3] < 0.5 \
+           or tmp_s[0, 0] <= 2*tmp_s[0, 1] or tmp_s[0, 1] <= tmp_s[0, 2] or 2*tmp_s[0, 2] \
+           <= tmp_s[0, 3]:
             s_ = s.clone()
         return s_
 
     def replay_buffer(self, s, a, r, s_):
         s = s.cpu()
         s_ = s_.cpu()
-        s = np.array(s).astype(float).round(2).reshape(1, 2)
-        s_ = np.array(s_).astype(float).round(2).reshape(1, 2)
+        s = np.array(s).astype(float).round(2).reshape(1, 4)
+        s_ = np.array(s_).astype(float).round(2).reshape(1, 4)
         a = np.array(a).astype(int).reshape(1, 1)
         r = np.array(r).astype(float).reshape(1, 1)
         transition = np.hstack((s, a, r, s_))
@@ -153,7 +181,7 @@ class DQN():
             # r = -np.log(max(s_error_, 1e-4)) - 3
             return r
         else:
-            r = -10
+            r = -5
             return r
 
     def learn(self):
@@ -185,8 +213,8 @@ class DQN():
 
 if __name__ == '__main__':
     # Define object and initial geometry
-    obj = np.array([2.65, 1.95]).reshape(1, 2)
-    init = np.array([3.00, 2.20]).reshape(1, 2)
+    obj = np.array([7.2, 3.4, 2.8, 1.3]).reshape(1, 4)
+    init = np.array([8.0, 2.8, 1.8, 0.7]).reshape(1, 4)
     dqn = DQN()
     s_tmp = torch.cuda.FloatTensor(init) if torch.cuda.is_available() else torch.Tensor(init)
 
