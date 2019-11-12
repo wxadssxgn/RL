98637010ee46879405bebb9ed0297714b5abed99
wxadssxgn <19493864@qq.com>
2019-11-12 16:08:44 +0800

    1

--- iterations_steps_plot.py
+++ iterations_steps_plot.py
@@ -4,14 +4,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = np.load('ring100.npy')
+a = np.load('steps_gap_ring_q_learning.npy')
 
 fig = plt.figure(figsize=(9,5))
-x = np.arange(1, 101)
-plt.plot(x, a, linewidth=2.5 ,color='black')
+x = np.arange(1, 501)
+plt.plot(x, a, linewidth=1.5 ,color='black')
 plt.grid(True, linestyle='-.')
-plt.xlim((0, 100))
-# plt.yscale('log')
+# plt.xlim((0, 500))
+plt.yscale('log')
 plt.xlabel('Iterations', size=16)
 plt.ylabel('Steps', size=16)
 plt.show()
